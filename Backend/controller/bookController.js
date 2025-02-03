a3d85bcfcf87d926ebcce5b4f719f2bd55af8e0b
Nasir <heyynasir@gmail.com>
2025-02-03 08:24:41 -0800

    edit1

--- Backend/controller/bookController.js
+++ Backend/controller/bookController.js
@@ -3,6 +3,7 @@ import Book from '../models/bookModel.js'
 export const getBook = async(req , res)=>{
     try{
         const book = await Book.find()
+        res.send(book)
         res.status(200).json(book)
     }catch(err){
         console.log("Error : ",err)
